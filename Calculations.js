6de964b58c5d7ad822414757e99e01ed45c45a5a
usman133 <usmankiani26@gmail.com>
2022-03-25 11:44:38 +1300

    Temp conversion - initial

---
+++ Calculations.js
@@ -0,0 +1,4 @@
+document.getElementById("celciusInput").addEventListener("input", e => {
+    let celcius = e.target.value;
+    document.getElementById("fahrenOutput").innerHTML = (celcius * 1.8) + 32;
+  })
