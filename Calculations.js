7cdde6268a113e95220a6edafef690563caaa57b
usman133 <usmankiani26@gmail.com>
2022-03-25 12:27:51 +1300

    Temp calc

--- Calculations.js
+++ Calculations.js
@@ -2,3 +2,4 @@ document.getElementById("celciusInput").addEventListener("input", e => {
     let celcius = e.target.value;
     document.getElementById("fahrenOutput").innerHTML = (celcius * 1.8) + 32;
   })
+
